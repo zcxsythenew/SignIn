71c0216f04be9a217baf3ff576080467126ffe40
Zcxsythenew <zcxsythenew@outlook.com>
2018-12-26 07:30:02 +0800

    启动时在命令行提示。

--- SignIn/app.js
+++ SignIn/app.js
@@ -80,4 +80,5 @@ app.set('port', process.env.PORT || 8000);
 
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
+    console.log('Express server listening on port ' + server.address().port);
 });
